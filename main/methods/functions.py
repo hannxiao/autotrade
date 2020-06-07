fcad4161aa8bd0ea3c379ccd8805d9e48a64723e
Xiao Han <44252261+hannxiao@users.noreply.github.com>
2020-06-06 21:10:04 -0400

    Update functions.py

--- main/methods/functions.py
+++ main/methods/functions.py
@@ -14,9 +14,8 @@ from . import indicators, strategies
     
 class Generic(metaclass=ABCMeta):
     def __init__(self, symbol, start, end, interval='1d'):
-        self._test = str(type(symbol))
+        self._test =  symbol
 # =============================================================================
-# 
 #         if symbol and symbol[0].isdigit(): # China market, default interval=1d
 #             pro = ts.pro_api()
 #             df = pro.daily(ts_code=symbol, start_date=start.replace('-', ''),
